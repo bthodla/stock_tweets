89fb9a271a97729d1a290a6b66458e0f11f868ce
Thodla (Murthy) <BMURTHY@LORDABBETT.COM>
2017-07-07 12:09:27 -0400

    Initial commit

---
+++ bt_utils.py
@@ -0,0 +1,65 @@
+import sys
+
+def prime(n: int) -> int:
+    a, s = 1, []
+    while a <= n:
+        if is_prime (a):
+            s.append(a)
+        a += 1
+    return s
+
+def is_prime(n: int) -> int:
+    is_prime = True
+    b, c = pow(n, 1 / 2), 2
+    while c <= b:
+        if n % c == 0:
+            is_prime = False
+            break
+        else:
+            is_prime = True
+        c += 1
+    return is_prime
+
+def fibonacci_series(n: int) -> int:
+    a, b, s = 0, 1, []
+    assert isinstance(n, int)
+    while a <= n:
+        # print(a, end=' ')
+        s.append(a)
+        a, b = b, a + b
+    return s
+
+def fibonacci (n: int) -> int:
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n -1) + fibonacci(n - 2)
+
+
+def is_fib_number (n: int) -> int:
+    if (n in fibonacci_series(n)):
+        return True
+    else:
+        return False
+
+def fib_prime_matches (n: int) -> int:
+    return sorted ((set (fibonacci_series (n)) & set (prime (n))))
+
+def factorial (n: int) -> int:
+    if (n == 0):
+        return 1
+    else:
+        recurse = factorial(n - 1)
+        result = n * recurse
+        return result
+
+def find (word, letter):
+    index = 0
+    while index < len (word):
+        if word [index] == letter:
+            return index
+        else:
+            index = index + 1
+    return -1
---
+++ main.py
@@ -0,0 +1,16 @@
+import bt_utils
+
+if __name__ == '__main__':
+    # n, fib_list, prime_list = 0, [], []
+    #
+    # n = input('Enter a number: ')
+    # n = int(n)
+    # fib_list = fibonacci_series(n)
+    # prime_list = prime(n)
+    # print('Fibonacci Numbers: ' + str(len(fib_list)) + ' - ', str(fib_list))
+    # print('Prime Numbers: ' + str(len(prime_list)) + ' - ', str(prime_list))
+    # print(str(n) + ' is a Fibonacci Number:' + str(is_fib_number(n)))
+    # print(fib_prime_matches(n))
+    # print('Factorial of ' + str(10) + ':' + str(factorial(10)))
+    # print('10th Fibonacci :' + str(fibonacci(10)))
+    print(bt_utils.find('Bhasker Thodla', 'e'))
